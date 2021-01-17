c6d82f5dbcf347b5b417f6d7cb31eb1656b1a313
Kevin Daniel <Dsanchez0397@gmail.com>
2021-01-17 14:34:49 -0600

    Generar contraseña

--- models/registroEmpleadoModel.php
+++ models/registroEmpleadoModel.php
@@ -7,12 +7,14 @@ class registroEmpleadoModel extends Model_{
     }
     
     public function insert($datos){
-        $query = $this->db->connect()->prepare('INSERT INTO Empleado (Nombre, Apellidos, Direccion, Telefono, Puesto, Correo) VALUES (:Nombre, :Apellidos, :Direccion, :Telefono, :Puesto, :Correo)');
+      $query = $this->db->connect()->prepare('INSERT INTO Empleado (Nombre, Apellidos, Direccion, Telefono, Puesto, Correo, Contrasena) 
+                                              VALUES (:Nombre, :Apellidos, :Direccion, :Telefono, :Puesto, :Correo, :Pass)');
         $query->execute(['Nombre'=>$datos['nombre'],
                          'Apellidos'=>$datos['apellidos'], 
                          'Direccion'=>$datos['direccion'],
                          'Telefono'=>$datos['telefono'],
                          'Puesto'=>$datos['puesto'],
-                         'Correo'=>$datos['email']]);
+                         'Correo'=>$datos['email'],
+                         'Pass'=>substr($datos['nombre'], 0, 3).substr($datos['telefono'], -4)]);
     }
 }
