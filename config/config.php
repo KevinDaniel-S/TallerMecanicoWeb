5b4f44d609a891dccf2af8db97e358fb6429da02
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-01-17 20:55:26 -0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('URL', 'https://pure-sierra-09569.herokuapp.com/');
+define('PAR', 1);
+define('HOST', 'nl-srv-web326.main-hosting.eu');
+define('DB', 'u117281852_w1204');
+define('USER', 'u117281852_w1204');
+define('PASSWORD', 'ProgramacionWeb62$');
+define('CHARSET', 'utf8mb4');
+
+?>
