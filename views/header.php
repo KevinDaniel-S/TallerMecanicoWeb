e83e51248fcae732ba167ab25bc046a3d5d7dedc
Kevin Daniel <Dsanchez0397@gmail.com>
2021-01-17 21:58:35 -0600

    htaccess

--- views/header.php
+++ views/header.php
@@ -5,7 +5,7 @@
     <title>Taller</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="shortcut icon" 
-          href="https://github.com/KevinDaniel-S/TallerMecanicoWeb/blob/master/assets/favicon.ico" 
+          href="assets/favicon.ico"
           type="image/x-icon">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
