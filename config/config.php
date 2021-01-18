1b5376c15a382283f3b69a523c9b8b58cebf8118
Kevin Daniel <Dsanchez0397@gmail.com>
2021-01-18 04:33:50 -0600

    Change host

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 define('URL', 'https://pure-sierra-09569.herokuapp.com');
 define('PAR', 0);
-define('HOST', 'nl-srv-web326.main-hosting.eu');
+define('HOST', '35.170.244.239');
 define('DB', 'u117281852_w1204');
 define('USER', 'u117281852_w1204');
-define('PASSWORD', 'ProgramacionWeb62');
+define('PASSWORD', 'ProgramacionWeb62$');
 define('CHARSET', 'utf8mb4');
 ?>
