bb11c37913079ce84ee638ce9b46393cf939f151
Kevin Daniel <Dsanchez0397@gmail.com>
2021-01-20 08:16:49 -0600

    Fix footer

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 </div>
     <div class="text-left py-3 bg-secondary"
         style="opacity: 0.9;">
-        <span class="text-black">© 2020 Copyright:</span>  
+        <span class="text-black">© 2020 Copyright: <b>Equipo 4</b> Programación web 12:00-13:00</span>  
     </div>
 </body>
 </html>
